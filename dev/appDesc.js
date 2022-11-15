03bafffbe9bbc9901fe8f87b82f9a0e8a0196fa0
ctamark <51331545+ctamark@users.noreply.github.com>
2022-11-15 10:22:02 +0900

    Add files via upload

---
+++ dev/appDesc.js
@@ -0,0 +1,19 @@
+
+const playkind_earCleaning = 0
+const playkind_acne = 1
+const playknd_waxing = 2 
+
+//---------------------
+const playstate_waitStart = 0
+const playstate_ready  = 0
+
+const playstate_prepare = 1
+
+const playstate_treatment = 2
+
+const playstate_finish = 3
+//--------------------
+
+
+var gSelectedPlay = 0
+
